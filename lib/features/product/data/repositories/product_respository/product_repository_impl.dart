f439f46bde0c608e0c87b8450d8bc026c4ea03ad
Tee Yu June <56582497+yujune@users.noreply.github.com>
2024-03-10 21:04:46 +0800

    chore: move repository interface to domain folder.

--- lib/features/product/data/repositories/product_respository/product_repository_impl.dart
+++ lib/features/product/data/repositories/product_respository/product_repository_impl.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 import 'package:flutter_clean_architecture/config/network/chopper_config.dart';
 import 'package:flutter_clean_architecture/core/data/models/paginated_response/paginated_response.dart';
 import 'package:flutter_clean_architecture/features/product/data/models/product/product.dart';
-import 'package:flutter_clean_architecture/features/product/data/repositories/product_respository/product_repository.dart';
 import 'package:flutter_clean_architecture/features/product/data/services/product_service.dart';
+import 'package:flutter_clean_architecture/features/product/domain/repositories/product_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'product_repository_impl.g.dart';
